37362706f50300575e0eb4a11aff6551d5b3a060
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-11-18 10:13:09 -0600

    created a function in the routes views.js

--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
+const { Router } = require("express");
 const path = require("path");
 
+module.exports = function(app){
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(__dirname, "../public.index.html"))
+    });    
 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.hmtl"))
@@ -10,3 +15,5 @@ router.get("/exercise", (req, res) => {
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.hmtl"))
 })
+
+module.exports = Router;
