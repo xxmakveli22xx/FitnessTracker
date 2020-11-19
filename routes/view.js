eaf60f12aba9ed9b03e3f7a8ac9bfc808727868f
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-11-18 22:18:52 -0600

    updated

--- routes/view.js
+++ routes/view.js
@@ -2,11 +2,6 @@ const router = require("express").Router();
 const { Router } = require("express");
 const path = require("path");
 
-module.exports = function(app){
-    app.get("/", (req,res) => {
-        res.sendFile(path.join(__dirname, "../public.index.html"))
-    });    
-
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.hmtl"))
 });
@@ -16,5 +11,5 @@ router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.hmtl"))
 });
 
-}
+
 module.exports = Router;
