4b2a0717b0701d306519f84e13839419064091a5
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-11-18 22:08:46 -0600

    working on the api routes

--- routes/api.js
+++ routes/api.js
@@ -12,18 +12,45 @@ router.post("/api/workouts", (req, res) =>{
     })
 })
 //router.put
-router.put("/api/workouts/:id");
-//findbyid
+
+
+ router.put("/api/workouts/:id", function (req, res) {
+   Tracker.findByIdAndUpdate(
+        req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true}
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 
 
 //router.get
-router.get("/api/workouts");
+
+router.get("/api/workouts", function(req,res){
+    Tracker.find({})
+    .then(function(dbWorkout){
+        res.json(dbWorkout);
+    })
+    .catch(function(err){
+        res.status(400).json(err);
+    });
+});
 
 //router.get range and distant
-router.get("/api/workouts/range")
+
+router.get("/api/workouts/range", function (req, res) {
+    Tracker.find({}).limit(10).then(data => res.json(data)
+    ).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 
 //router.delete
-router.delete("/api/workouts")
+//router.delete("/api/workouts")
 
 
 module.exports = router;
